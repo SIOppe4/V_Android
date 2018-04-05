e8b4eb62421a66cc228ef7d262392f6abb0bf891
Maxime <bouchetm@saintemarie-cholet.eu>
2018-04-05 08:35:18 +0200

    modification client partie graphique

--- app/src/main/java/app/tobat/fr/tobat/ClientActivity.java
+++ app/src/main/java/app/tobat/fr/tobat/ClientActivity.java
@@ -2,8 +2,13 @@ package app.tobat.fr.tobat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import java.io.Serializable;
 
 import app.tobat.fr.tobat.Model.Client;
 
@@ -19,7 +24,6 @@ public class ClientActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         client = (Client)intent.getSerializableExtra("client");
-
         setContentView(R.layout.client_activity);
 
         TextView prenom = (TextView) findViewById(R.id.prenom_client);
@@ -37,5 +41,12 @@ public class ClientActivity extends AppCompatActivity {
         TextView adresse = (TextView) findViewById(R.id.adresse_client);
         adresse.setText(client.getAdresse());
 
+    }
+
+    public void goToUpdateClient(View v){
+        Log.i("INFO MAX", "Clicked floating button");
+        Intent intent = new Intent (getApplicationContext(), updateClientActivity.class);
+        intent.putExtra("client",(Serializable) client);
+        startActivity(intent);
     }
 }
